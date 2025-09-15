6c0eee699d4b8ad15e20b4f312129f116d231153
jbeattie5768 <jbeattie5768@gmail.com>
2025-09-15 17:19:48 +0100

    deleted: Deleted and moved some files

--- win_setup/install/map_local_drive.bat
+++
@@ -1,9 +0,0 @@
-::::::::::::::::::::::::::::::::::::::::::::
-:: Drop into startup folder %APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup
-:: Also get to from run -> shell:startup
-::::::::::::::::::::::::::::::::::::::::::::
-
-IF EXIST c:\w_local_drive (
-subst W: c:\w_local_drive
-)
-
