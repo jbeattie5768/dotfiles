f9ff5d67925f46db4405954c7262d8b9b10dfb6a
jbeattie5768 <jbeattie5768@gmail.com>
2025-09-23 15:30:23 +0100

    feat: Added Powershell grep alias to PS profile

--- consoles/Microsoft.PowerShell_profile.ps1
+++ consoles/Microsoft.PowerShell_profile.ps1
@@ -4,21 +4,30 @@ oh-my-posh --init --shell pwsh --config ~/jb29_theme.omp.json | Invoke-Expressio
 
 # Reload profile with: . $PROFILE
 # View all Aliases with: Get-Alias
-# Create new Alias with: New-Alias <alias> <command> [-Option <options
-# <https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/new-alias?view=powershell-7.5>
 Set-Alias np "C:\windows\notepad.exe"
 Set-Alias npp "C:\Program Files\Notepad++\notepad++.exe"
+Set-Alias edit npp
 Set-Alias vi vim
 Set-Alias history Get-History
+
+function grep{
+    # Recurse from CWD for case-insensitive, e.g. grep *.md "admin rights"
+    # Similar to: ls -r *.md | cat | findstr "admin rights"
+    Get-ChildItem -Path .\ -Recurse -Include $args[0] | Select-String -Pattern $args[1]
+}
+
+function wexe { (get-command $args[0]).Path }  # Equivalent to cmd `where`
 
 function touch {New-Item -ItemType File -Name $args[0]}
 
 # See: get-help get-process -full
 # Alternative is: <https://github.com/gsass1/NTop>
 function top {
+    $NumProcesses = 15
+    $SleepSec = 2
     While(1) {
-        Get-Process | Sort-Object -Descending -Property cpu | Select-Object -First 15 | Format-Table -a
-        Start-Sleep 2
+        Get-Process | Sort-Object -Descending -Property cpu | Select-Object -First $NumProcesses | Format-Table -a
+        Start-Sleep $SleepSec
         Clear-Host
     }
 }
