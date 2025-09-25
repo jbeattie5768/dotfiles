12a1c1f98c73d2df8fbdbafebbb85d85c5083edf
jbeattie5768 <jbeattie5768@gmail.com>
2025-09-25 12:43:59 +0100

    Feat: Added Terminal Icons and Updated PowerShell Profile

--- consoles/Microsoft.PowerShell_profile.ps1
+++ consoles/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,19 @@
+# ############################
+# Prompt and Completions
+# ############################
 oh-my-posh --init --shell pwsh --config ~/jb29_theme.omp.json | Invoke-Expression
 (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
 (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
 
+# ############################
+# Modules
+# ############################
+# Assumes `Import-Module -Name Terminal-Icons` already done
+Import-Module -Name Terminal-Icons
+
+# ############################
+# Aliases and Functions
+# ############################
 # Reload profile with: . $PROFILE
 # View all Aliases with: Get-Alias
 Set-Alias np "C:\windows\notepad.exe"
@@ -10,15 +22,17 @@ Set-Alias edit npp
 Set-Alias vi vim
 Set-Alias history Get-History
 
-function grep{
+function lw { Get-ChildItem | Format-Wide }  # gci | fw
+
+function grep {
     # Recurse from CWD for case-insensitive, e.g. grep *.md "admin rights"
     # Similar to: ls -r *.md | cat | findstr "admin rights"
     Get-ChildItem -Path .\ -Recurse -Include $args[0] | Select-String -Pattern $args[1]
 }
 
-function wexe { (get-command $args[0]).Path }  # Equivalent to cmd `where`
+function wexe { (get-command $args[0]).Path}  # Equivalent to cmd `where`
 
-function touch {New-Item -ItemType File -Name $args[0]}
+function touch { New-Item -ItemType File -Name $args[0] }
 
 # See: get-help get-process -full
 # Alternative is: <https://github.com/gsass1/NTop>
@@ -31,3 +45,5 @@ function top {
         Clear-Host
     }
 }
+
+# ############################
